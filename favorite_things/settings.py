a0488bee3ada75b34d4318e4014aa063ae2898ac
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2019-08-22 12:48:11 +0100

    [master] Load static from root dir

--- favorite_things/settings.py
+++ favorite_things/settings.py
@@ -59,11 +59,11 @@ ALLOWED_HOSTS = ['favoritethings.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER':os.environ['DB_USER'],
-        'PASSWORD':os.environ['DB_PASSWORD'],
-        'HOST':os.environ['DB_HOST'],
-        'PORT':os.environ['DB_PORT']
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER':os.environ.get('DB_USER', ''),
+        'PASSWORD':os.environ.get('DB_PASSWORD', ''),
+        'HOST':os.environ.get('DB_HOST', ''),
+        'PORT':os.environ.get('DB_PORT', ''),
     }
 }
 
